de75c83bedbf49fb9e831e17af68d38e026ac03c
o.bervinov <o.bervinov@corp.mail.ru>
2022-11-04 15:37:54 +0400

    new file:   CHANGELOG.md modified:   pyproject.toml new file:   setup.py deleted:    setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name = 'vault', 
+    version = '1.0.0',
+    description = 'This module contains a collection of methods for working with vault.',
+    py_modules = ["vault"],
+    package_dir = {'':'src'},
+    author = 'Oleg Bervinov',
+    author_email = 'unknown-312@ya.ru',
+    long_description = open('README.md').read() + '\n\n' + open('CHANGELOG.md').read(),
+    long_description_content_type = "text/markdown",
+    url='https://github.com/obervinov/vault-package',
+    include_package_data=True,
+    classifiers  = [
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        "License :: OSI Approved :: BSD License",
+        'Intended Audience :: Developers',
+        'Intended Audience :: Other Audience',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'Topic :: Text Processing',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: OS Independent',
+    ],
+    keywords = ['vault', 'secure'],    
+)
