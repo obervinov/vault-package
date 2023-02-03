65e3d7b4fa24da6cfe575d2d3b08727b77c801cb
Bervinov Oleg <oleg.bervinov@bostongene.com>
2023-02-03 12:17:02 +0400

    updated pyproject.toml and setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name = 'vault', 
-    version = '1.0.0',
+    version = '1.0.1',
     description = 'This module contains a collection of methods for working with vault.',
     py_modules = ["vault"],
     package_dir = {'':'src'},
     author = 'Oleg Bervinov',
-    author_email = 'unknown-312@ya.ru',
+    author_email = 'obervinov@mail.ru',
     long_description = open('README.md').read() + '\n\n' + open('CHANGELOG.md').read(),
     long_description_content_type = "text/markdown",
     url='https://github.com/obervinov/vault-package',
